de281999eab8ddb07660b316fd56c5dc68150dde
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-09-17 14:02:39 +0800

    make static

--- hash/sha256_hash.c
+++ hash/sha256_hash.c
@@ -4,17 +4,19 @@
 
 #include "../include/hash/sha256_hash.h"
 
-bool hash_function(const uint8_t *input1, uint32_t input1_len, const uint8_t *input2,
-                       uint32_t input2_len, const uint8_t *input3, uint32_t input3_len,
-                       const uint8_t *input4, uint32_t input4_len, uint8_t *output) {
+static bool hash_function(const uint8_t *input1, uint32_t input1_len,
+                          const uint8_t *input2, uint32_t input2_len,
+                          const uint8_t *input3, uint32_t input3_len,
+                          const uint8_t *input4, uint32_t input4_len,
+                          uint8_t *output) {
 
     SHA256_CTX ctx;
     return SHA256_Init(&ctx) &&
-            (input1 == NULL || SHA256_Update(&ctx, input1, input1_len)) &&
-            (input2 == NULL || SHA256_Update(&ctx, input2, input2_len)) &&
-            (input3 == NULL || SHA256_Update(&ctx, input3, input3_len)) &&
-            (input4 == NULL || SHA256_Update(&ctx, input4, input4_len)) &&
-            SHA256_Final(output, &ctx);
+           (input1 == NULL || SHA256_Update(&ctx, input1, input1_len)) &&
+           (input2 == NULL || SHA256_Update(&ctx, input2, input2_len)) &&
+           (input3 == NULL || SHA256_Update(&ctx, input3, input3_len)) &&
+           (input4 == NULL || SHA256_Update(&ctx, input4, input4_len)) &&
+           SHA256_Final(output, &ctx);
 }
 
 bool DRBG_HASH_SHA256_conf(DRBG_HASH_CONF *conf) {
